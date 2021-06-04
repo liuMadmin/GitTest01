ca6722766f770a8bca0214c1e13fd76b819ba86d
LM <LM@163.com>
2021-06-05 01:18:26 +0800

    GitTest02 version2

---
+++ src/com/javacto/action/testAction.java
@@ -0,0 +1,16 @@
+package com.javacto.action;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+@WebServlet("/test.do")
+public class testAction extends HttpServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("你好");
+    }
+}
