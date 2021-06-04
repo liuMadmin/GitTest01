285a35b7f0b8213bf93193c65400578ad36de893
LM <LM@163.com>
2021-06-05 01:33:14 +0800

    GitTest01 version3

--- src/com/javacto/action/HelloAction.java
+++ src/com/javacto/action/HelloAction.java
@@ -7,10 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import java.io.IOException;
 
-@WebServlet("/test.do")
-public class testAction extends HttpServlet {
+@WebServlet("/hello.do")
+public class HelloAction extends HttpServlet {
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        System.out.println("你好");
+        System.out.println("hello");
     }
 }
